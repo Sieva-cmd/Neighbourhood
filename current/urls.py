a2c1d649553791c2f0d8c3606b7d4479a186ff14
Sieva-cmd <musyokasieva@gmail.com>
2022-04-15 18:36:16 +0300

    add authentication

--- current/urls.py
+++ current/urls.py
@@ -3,4 +3,7 @@ from django.urls import path, re_path
 
 urlpatterns =[
     path('',views.home ,name='home'),
+    re_path(r'register/',views.register_request, name="register"),
+    re_path(r'login/', views.login_request, name="login"),
+    re_path(r'logout', views.logout_request, name= "logout"),
 ]
